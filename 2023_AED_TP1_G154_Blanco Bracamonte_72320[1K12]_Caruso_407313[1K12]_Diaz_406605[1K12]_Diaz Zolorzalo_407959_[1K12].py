55d32259ed58a5e4fb3fed482059969c8bbbdc1b
federicodzn <federicodzn@gmail.com>
2023-05-06 15:50:09 -0300

    TP1 Posta para entregar

---
+++ 2023_AED_TP1_G154_Blanco Bracamonte_72320[1K12]_Caruso_407313[1K12]_Diaz_406605[1K12]_Diaz Zolorzalo_407959_[1K12].py
@@ -0,0 +1,54 @@
+# Ingreso de datos
+patente = input("Ingrese la patente del vehículo: ")
+tipo_vehiculo = int(input("Ingrese el tipo de vehículo (0: motocicleta, 1: automóvil, 2: camión): "))
+forma_pago = int(input("Ingrese la forma de pago (1: manual, 2: telepeaje): "))
+pais = int(input("Ingrese el país donde se encuentra la cabina (0: Argentina, 1: Bolivia, 2: Brasil, 3: Paraguay, 4: Uruguay): "))
+distancia = float(input("Ingrese la distancia recorrida desde la última cabina en kilómetros: "))
+print()
+
+# Validación de la patente
+if len(patente) == 7:
+    if patente[:2].isalpha() and patente[2:5].isdigit() and patente[5:].isalpha():
+        print("El auto corresponde a el pais Argentina")
+    elif patente[:3].isalpha() and patente[3:].isdigit():
+        print("El auto corresponde a el pais Uruguay")
+    elif patente[:2].isalpha() and patente[2:].isdigit():
+        print("El auto corresponde a el pais Bolivia")
+    elif patente[:4].isalpha() and patente[4:].isdigit():
+        print("El auto corresponde a el pais Paraguay")
+    elif patente[:3].isalpha() and patente[3:4].isdigit() and patente[4:5].isalpha() and patente[5:].isdigit():
+        print("El auto corresponde a el pais Brasil")
+    else:
+        print("El vehículo es de Otro pais")
+else:
+    print("El vehículo es de Otro pais")
+
+# Cálculo del importe básico
+if pais == 2:
+    importe_base = 400
+elif pais == 1:
+    importe_base = 200
+else:
+    importe_base = 300
+
+if tipo_vehiculo == 0:
+    importe_base *= 0.5
+elif tipo_vehiculo == 2:
+    importe_base *= 1.6
+
+print("El importe básico a pagar por el vehículo es:", importe_base)
+
+# Aplicación del descuento por telepeaje
+if forma_pago == 2:
+    importe_final = importe_base * 0.9
+else:
+    importe_final = importe_base
+
+print("El valor final del ticket es:", importe_final)
+
+# Cálculo del valor promedio pagado por kilómetro recorrido
+if distancia != 0:
+    valor_promedio_por_kilometro = round(importe_final / distancia, 2)
+    print("El valor promedio pagado por kilómetro recorrido es:", valor_promedio_por_kilometro)
+else:
+    print("No aplica el calculo del promedio porque los kilometros recorridos son 0")
